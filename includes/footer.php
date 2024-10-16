c08388dc08740e5929da3d94fbb9f6110e5fc7bb
Shreyashslakamble <shreyashslakamble101@gmail.com>
2024-10-16 12:41:18 +0530

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Vishwakarma University (Sem-II Mini Project)</b>
+    </div>
+    <strong> Apartment Visitor Management System </strong>
+  </footer>
